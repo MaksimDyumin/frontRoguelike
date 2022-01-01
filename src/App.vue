b40ed3634cf4d23adeadac17c5374d4cc2d9df96
MaksimDyumin <68166387+MaksimDyumin@users.noreply.github.com>
2022-01-01 19:46:05 +0300

    Коммит для залива на github

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Canvas />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Canvas from './components/Canvas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Canvas
   }
 }
 </script>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <button @click="render"></button>
+        <div class="canvasDiv">
+            <canvas id="chart"></canvas>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Canvas',
+
+  methods:{
+      render(){
+          const chart = document.getElementById('chart')
+          let ctx = chart.getContext('2d');
+          chart.height = 480;
+          chart.width  = 640;
+          ctx.clearRect(0, 0, chart.width, chart.height);
+          ctx.beginPath();
+            ctx.moveTo(10, 15);
+            ctx.bezierCurveTo(75, 55, 175, 20, 250, 15);
+            ctx.moveTo(10, 15);
+            ctx.quadraticCurveTo(100, 100, 250, 15);
+            ctx.stroke();
+        }
+    }
+}
+</script>
+
+<style scoped>
+.canvasDiv{
+    border: 1px solid black;
+}
+</style>
